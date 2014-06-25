c7e1d7526d350d42cedae292da8a1669db33ad5a
Jon Flash <funkswing@gmail.com>
2014-06-25 15:41:20 -0400

    Setup of ubertool_orca submodule app

--- stylesheets/scripts_header_hh.js
+++ stylesheets/scripts_header_hh.js
@@ -24,11 +24,13 @@ $(document).ready(function() {
         $('#about_text').stop().animate({width:0}, {duration: 500, queue: false}).fadeTo(500,0);
     }
   );
+  // Links
   $('.articles a').hover(function(){
     $(this).stop().animate({ color:'#A31E39' },500);
   }, function(){
     $(this).stop().animate({ color:'#485C5A' },500);
   });
+  // Index link
   $('#topheader_p p, .logreg').hide();
   $('.index_link').click(function(e) {
     e.preventDefault();
@@ -39,16 +41,44 @@ $(document).ready(function() {
     }, 500);
     $('#topheader_p p, .logreg').fadeIn(500);
   });
-  // BlockUI setup
-  var browserWidth = $(window).width();
-  var browserHeight = $(window).height();
-  var winleft = (browserWidth / 2) - 220 + "px";
-  var wintop = (browserHeight / 2) - 30 + "px";
-  $("a[class^='fadeQAQC']").click(function () {
-    $.blockUI({
-      css:{ "top":""+wintop+"", "left":""+winleft+"", "padding": "30px 20px", "width": "400px", "height": "60px", "border": "0 none", "border-radius": "4px", "-webkit-border-radius": "4px", "-moz-border-radius": "4px", "box-shadow": "3px 3px 15px #333", "-webkit-box-shadow": "3px 3px 15px #333", "-moz-box-shadow": "3px 3px 15px #333" },
-      message: '<h2 class="popup_header">Processing Request...</h2><br/><img src="/images/loader.gif" style="margin-top:-16px">',
-      fadeIn:  500
+  // BlockUI on QAQC
+  $("a[class^='fadeQAQC']").click(function (e) {
+    e.preventDefault();
+    $(document).ajaxStart(function(){
+      $.blockUI({
+        css:{ "top":""+wintop+"", "left":""+winleft+"", "padding": "30px 20px", "width": "400px", "height": "60px", "border": "0 none", "border-radius": "4px", "-webkit-border-radius": "4px", "-moz-border-radius": "4px", "box-shadow": "3px 3px 15px #333", "-webkit-box-shadow": "3px 3px 15px #333", "-moz-box-shadow": "3px 3px 15px #333" },
+        message: '<h2 class="popup_header">Processing QA/QC...</h2><br/><img src="/images/loader.gif" style="margin-top:-16px">',
+        fadeIn:  500
+      });
     });
+      var retStatus;
+      $.ajax({
+          cache: false,
+          complete: function(xhr) { 
+            retStatus = xhr.status;
+          },
+          success: function() {
+            window.location.href = model+"_qaqc.html";
+          },
+          error: function() {
+            $.unblockUI();
+            alert('There is a problem about your submission.')
+          }
+        });
+      if(retStatus == 200)
+          return false;
+      else
+          return true;
+  });
+  // Save Metadata
+  $('#metaDataTxtArea').prop('disabled', true);
+  $('#metaDataToggle').change(function() {
+    if ($('#metaDataToggle').prop( "checked" )) {
+      $('#metaDataTxtArea').prop('disabled', false);
+      $('#metaDataDiv').slideDown();
+    } else {
+      $('#metaDataDiv').slideUp();
+      $('#metaDataTxtArea').prop('disabled', true);
+    }
   });
 });
